5f5d53d45bc944ffcfebb5f0d196b693256e96bc
Josh Wright <madewithlinux.josh@gmail.com>
2019-06-03 22:50:44 -0500

    WIP image writing kinematics simulation

---
+++ image_kinematics.py
@@ -0,0 +1,36 @@
+from gcode import CartesianKinematics
+from PIL import Image
+from PIL import ImageStat
+from PIL import ImageDraw
+
+
+class ImageKinematics(CartesianKinematics):
+    """writes to an image"""
+
+    def __init__(self, size):
+        super(ImageKinematics, self).__init__()
+        self.size = size
+        self.position = (0, 0)
+        self.lines = []
+
+    def move(self, x, y):
+        super().move(x, y)
+        self.lines.append((self.position, (x, y)))
+        self.position = (x, y)
+
+    def point_to_image_coordinates(self, p):
+        x, y = p
+        # TODO transform point
+        return x,y
+
+    def to_file(self, filename):
+        super(ImageKinematics, self).to_file(filename)
+        filename += ".png"
+        # TODO write to png file
+        im = Image.new('RGB', self.size, (255, 255, 255))
+        draw = ImageDraw.Draw(im)
+        for p0, p1 in self.lines:
+            p0 = self.point_to_image_coordinates(p0)
+            p1 = self.point_to_image_coordinates(p1)
+            draw.line([p0, p1], fill=(127, 127, 127), width=4)
+        im.save(filename)
